ebd29db2aa58169c74020028b84710906f368b6c
peppe-p <ombrachiara96@gmail.com>
2021-05-13 21:56:46 +0200

    JSnack 3

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//JSnack 1 Blocco 6
+//JSnack 1 Blocco 6 //////////////////////
 //Creare un array di oggetti, ogni oggetto descriverà una bici da corsa con le seguenti proprietà:
 //nome e peso.
 let biciList = [
@@ -67,28 +67,29 @@ console.log(pesoMinore());
 
 
 
-//JSnack 2 Blocco 6
+
+//JSnack 2 Blocco 6 ////////////////////////////////////////////
 //Creare un array di oggetti di squadre di calcio.Ogni squadra avrà diverse proprietà:
 //nome, punti fatti, falli subiti.
 //Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
-var squadre = [
+var teams = [
     {
-        nome: "Palermo",
+        team: "Palermo",
         punti: 0,
         falli_subiti: 0
     },
     {
-        nome: "Catania",
+        team: "Catania",
         punti: 0,
         falli_subiti: 0
     },
     {
-        nome: "Roma",
+        team: "Roma",
         punti: 0,
         falli_subiti: 0
     },
     {
-        nome: "Napoli",
+        team: "Napoli",
         punti: 0,
         falli_subiti: 0
     },
@@ -99,9 +100,36 @@ function rndmN() {
     return parseInt(Math.random() * 100) + 1;
 }
 
-for (let i = 0; i < squadre.length; i++) {
-    squadre[i].punti = rndmN();
-    squadre[i].falli_subiti = rndmN();
+for (let i = 0; i < teams.length; i++) {
+    teams[i].punti = rndmN();
+    teams[i].falli_subiti = rndmN();
 
 }
+
 //Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+const [palermo, catania, roma, napoli] = teams;
+const newTeams = [];
+for (let i = 0; i < teams.length; i++) {
+    let team = teams[i];
+    delete team.punti;
+    newTeams.push(team);
+}
+
+console.log(newTeams);
+
+
+
+
+// JSNack 3 Blocco 6 /////////////
+//Scrivere una funzione che accetti tre argomenti, un array e due numeri(a più piccolo di b).
+//La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+const spesa = ["Olio", "Pasta", "Gelato", "Latte", "Carne", "Mele", "Banane"];
+function takeNumbers(lista, min, max) {
+    const risultato = lista.filter((el, i) => {
+        let result = i >= min && i <= max;
+        return result;
+    })
+    return risultato;
+}
+
+console.log(takeNumbers(spesa, 2, 4));
